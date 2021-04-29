d896e46cef93d3ada5c9e6412c33be0d9551969a
Straub Levente <slevy57@gmail.com>
2021-04-29 12:31:57 +0200

    this is the jurassic park, not phonebook

--- src/main/java/hu/nive/ujratervezes/jurassic/JurassicPark.java
+++ src/main/java/hu/nive/ujratervezes/jurassic/JurassicPark.java
@@ -1,5 +1,9 @@
 package hu.nive.ujratervezes.jurassic;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class JurassicPark {
 
     private String dbUrl;
@@ -12,4 +16,30 @@ public class JurassicPark {
         this.dbPassword = dbPassword;
     }
 
+    public Connection getConnection() {
+        Connection connect = null;
+        try {
+            connect = DriverManager.getConnection(dbUrl, dbUser, dbPassword);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connect;
+    }
+
+    public List<String> checkOverpopulation(){
+
+        List<String> dinosResult = new ArrayList<>();
+        try (Connection connection = getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(
+                    "SELECT breed FROM dinosaur WHERE actual > expected ORDER BY BREED ASC");
+            ResultSet rs = statement.executeQuery();
+            while (rs.next()) {
+                dinosResult.add(rs.getString("breed"));
+            }
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+        return dinosResult;
+
+    }
 }
